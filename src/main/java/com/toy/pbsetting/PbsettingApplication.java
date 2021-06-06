084fd95cd5f924a3dced00ec1ff29699bfbb65b1
JongChan <kjc3590@gmail.com>
2021-06-06 19:33:45 +0900

    [Init] project init

---
+++ src/main/java/com/toy/pbsetting/PbsettingApplication.java
@@ -0,0 +1,13 @@
+package com.toy.pbsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbsettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbsettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/pbsetting/PbsettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.pbsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbsettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
