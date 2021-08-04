73e025533e1f2e9586f1a19adb9719ff3c24c215
JongChan <kjc3590@songareeit.com>
2021-08-04 21:36:26 +0900

    hello231313

--- src/main/java/com/toy/pbsetting/PbsettingApplication.java
+++ src/main/java/com/toy/pbsetting/PbsettingApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class PbsettingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("aaa :: " );
+		System.out.println("aaaaadasdadaaa :: " );
 		SpringApplication.run(PbsettingApplication.class, args);
 	}
 
