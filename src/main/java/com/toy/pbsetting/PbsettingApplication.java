cbdaf3c738ee0ab45e7be49c2b929b7d4522e35b
JongChan <kjc3590@songareeit.com>
2021-08-09 12:59:52 +0900

    [CI/CD] init

--- src/main/java/com/toy/pbsetting/PbsettingApplication.java
+++ src/main/java/com/toy/pbsetting/PbsettingApplication.java
@@ -1,16 +1,25 @@
 package com.toy.pbsetting;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class PbsettingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello :: " );
 		SpringApplication.run(PbsettingApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public ResponseEntity<?> get() {
+	    return ResponseEntity.ok("hello");
 	}
 
 }
