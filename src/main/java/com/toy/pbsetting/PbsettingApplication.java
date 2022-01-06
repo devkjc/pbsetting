8cf940c47f34f379e8ddc5c6c2f509d941612ea9
JongChan <kjc3590@songareeit.com>
2022-01-06 11:32:06 +0900

    [Setting] settingServer 로컬서버로 변경

--- src/main/java/com/toy/pbsetting/PbsettingApplication.java
+++ src/main/java/com/toy/pbsetting/PbsettingApplication.java
@@ -1,11 +1,8 @@
 package com.toy.pbsetting;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
